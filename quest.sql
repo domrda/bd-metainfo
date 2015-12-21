5ae0e80e02cde238ec837e6fb556751ae894f4a6
Denis Domrachev <domrachev.mail@gmail.com>
2015-12-21 23:53:28 +0300

    I forgot what it does

--- quest.sql
+++ quest.sql
@@ -1,19 +1,5 @@
-CREATE SEQUENCE table1_seq
-   START WITH 1
-   INCREMENT BY 1
-   NO MINVALUE
-   NO MAXVALUE
-   CACHE 1;--#
-
-CREATE SEQUENCE table2_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;--#
-
 CREATE TABLE table2 (
-    id integer DEFAULT nextval('table2_seq'::regclass),
+    id SERIAL,
     field1 integer,
     field2 integer,
     field3 integer,
@@ -24,10 +10,10 @@ CREATE TABLE table2 (
     UNIQUE(field1, field2),
     UNIQUE(field2, field3, field4),
     UNIQUE(field1)
-);--#
+);
 
 CREATE TABLE table1 (
-    id integer DEFAULT nextval('table1_seq'::regclass),
+    id SERIAL,
     field1 integer,
     field2 integer,
     field3 integer,
@@ -43,7 +29,7 @@ CREATE TABLE table1 (
     FOREIGN KEY (field1, field2) REFERENCES table2(field1, field2),
     FOREIGN KEY (field3, field4, field1) REFERENCES table2(field2, field3, field4),
     FOREIGN KEY (field4) REFERENCES table2(field1)
-);--#
+);
 
 INSERT INTO table2 (id, field1, field2, field3, field4, uniqueTable2Field) VALUES (1, 1, 1, 1, 1, 1), (2, 2, 2, 2, 2, 2), (3, 3, 3, 3, 3, 3);
 INSERT INTO table1 (id, field1, field2, field3, field4, uniqueTable1Field) VALUES (1, 1, 1, 1, 1, 1), (2, 2, 2, 2, 2, 2), (3, 3, 3, 3, 3, 3), (4, 4, 4, 4, 4, 4);
--- repository_shema.sql
+++ repository_shema.sql
@@ -1,33 +1,19 @@
-CREATE SEQUENCE main_seq
-   START WITH 1
-   INCREMENT BY 1
-   NO MINVALUE
-   NO MAXVALUE
-   CACHE 1;--#
-
-CREATE SEQUENCE rel_sec
-   START WITH 1
-   INCREMENT BY 1
-   NO MINVALUE
-   NO MAXVALUE
-   CACHE 1;--#
-
 CREATE TABLE bd (
-    id integer DEFAULT nextval('main_seq'::regclass) PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     database_name character varying(255) NOT NULL,
     table_name character varying(255) NOT NULL,
     attr_name character varying(255) NOT NULL,
     attr_type character varying(255) NOT NULL,
     is_pk boolean DEFAULT FALSE,
     pos integer DEFAULT NULL
-);--#
+);
 
 CREATE TABLE relations (
-  id integer DEFAULT nextval('rel_sec'::regclass) PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   unique_name character varying(255) NOT NULL,
   tableA character varying(255) NOT NULL,
   fieldA character varying(255) NOT NULL,
   tableB character varying(255) NOT NULL,
   fieldB character varying(255) NOT NULL,
   pos integer NOT NULL
-);--#
+);
